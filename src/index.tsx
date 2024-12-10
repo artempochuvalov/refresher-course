312ab1709eb6378cbc883d93e32c67c549612320
artempochuvalov <artempochuvalov@gmail.com>
2024-12-10 21:52:11 +0300

    redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import 'app/styles/index.scss';
 
 import { App } from 'app';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'shared/providers/theme';
@@ -11,11 +12,13 @@ const appContainer = document.getElementById('root');
 const root = createRoot(appContainer!);
 
 root.render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>
 );
