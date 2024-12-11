0f3ccbcd46860b26910ea4667355dacaece6ffe4
Pochuvalov Artem <74793775+artempochuvalov@users.noreply.github.com>
2024-12-11 21:33:22 +0300

    dev server setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,7 +63,7 @@ module.exports = {
             imports: 'never',
             exports: 'never',
         }],
-        'no-console': ['error', { allow: ['warn', 'error'] }],
+        'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
         'jsx-a11y/no-noninteractive-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-static-element-interactions': 'off',
