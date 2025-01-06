22f2eb5a82241557d67035b86c0eea09bb5ad6e9
artempochuvalov <artempochuvalov@gmail.com>
2025-01-06 17:08:31 +0300

    WiP

--- src/pages/About/ui/About.tsx
+++ src/pages/About/ui/About.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const AboutPage = memo(() => {
-    const { t } = useTranslation('about');
+    const { t } = useTranslation();
 
     return (
         <div>
