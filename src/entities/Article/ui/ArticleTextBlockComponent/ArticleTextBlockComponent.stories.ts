5c39bfdb5e1e3f2aba4e7237dbbe13a2073a5cfe
artempochuvalov <artempochuvalov@gmail.com>
2025-03-23 16:35:45 +0300

    article text block added

--- src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.stories.ts
+++ src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { ArticleBlockType } from '../../model/types/article';
 import { ArticleTextBlockComponent } from './ArticleTextBlockComponent';
 
 const meta = {
@@ -13,5 +14,22 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {},
+    args: {
+        block: {
+            id: '1',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок',
+            paragraphs: [
+                `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                In eros metus, aliquet a nisi at, rutrum aliquam lectus. Integer 
+                ornare dictum libero, a auctor dui bibendum eget. Nullam imperdiet ipsum
+                quis lacus posuere sodales. Cras non malesuada sapien. Phasellus consectetur
+                luctus sem, gravida elementum leo tempor ut. Nullam nec suscipit nulla, vitae
+                porta neque. Pellentesque habitant morbi tristique senectus et netus et malesuada
+                fames ac turpis egestas. Maecenas varius lorem vitae leo placerat placerat. Sed eu
+                molestie est, vitae efficitur justo. Nulla fermentum metus lorem, in sagittis
+                lorem pharetra at.`,
+            ],
+        },
+    },
 };
--- src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
+++ src/entities/Article/ui/ArticleTextBlockComponent/ArticleTextBlockComponent.tsx
@@ -1,21 +1,28 @@
 import { memo } from 'react';
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames';
+import { TextAtom } from 'shared/ui/TextAtom/TextAtom';
 
+import { ArticleTextBlock } from '../../model/types/article';
 import cls from './ArticleTextBlockComponent.module.scss';
 
 type ArticleTextBlockComponentProps = {
     className?: string;
+    block: ArticleTextBlock;
 };
 
 export const ArticleTextBlockComponent = memo((props: ArticleTextBlockComponentProps) => {
-    const { className } = props;
-
-    const { t } = useTranslation();
+    const { className, block } = props;
 
     return (
         <div className={classNames(cls.ArticleTextBlockComponent, {}, [className])}>
-            {t('text block')}
+            <TextAtom title={block.title} className={cls.ArticleTitle} />
+            {block.paragraphs.map((paragraph) => (
+                <TextAtom
+                    className={cls.ArticleParagraph}
+                    key={paragraph}
+                    text={paragraph}
+                />
+            ))}
         </div>
     );
 });
