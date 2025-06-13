e2deed6ff4205dcfd725fd746de00597f6382bdd
artempochuvalov <artempochuvalov@gmail.com>
2025-06-13 13:04:43 +0300

    articlePageSlice test added

---
+++ src/pages/Articles/model/slices/articlePageSlice.test.ts
@@ -0,0 +1,111 @@
+import { Article } from 'entities/Article';
+import { ArticleType } from 'entities/Article/model/types/article';
+
+import { fetchArticles } from '../services/fetchArticles/fetchArticles';
+import { ArticlesListSchema } from '../types/articlePageSchema';
+import { articlesListReducer } from './articlePageSlice';
+
+describe('articlePageSlice.test', () => {
+    test('fetch articles pending', () => {
+        const state: DeepPartial<ArticlesListSchema> = {
+            error: 'error',
+            isLoading: false,
+        };
+
+        expect(
+            articlesListReducer(
+                state as ArticlesListSchema,
+                fetchArticles.pending('', { page: 1, limit: 9 })
+            )
+        ).toEqual({
+            error: undefined,
+            isLoading: true,
+        });
+    });
+
+    test('fetch articles fulfilled', () => {
+        const state: DeepPartial<ArticlesListSchema> = {
+            isLoading: true,
+            hasMore: true,
+            entities: {},
+            ids: [],
+        };
+
+        const articles: Article[] = [
+            {
+                id: '1',
+                title: 'Random article',
+                subtitle: 'Subtitle',
+                user: {
+                    id: '1',
+                    username: 'random',
+                },
+                img: '',
+                views: 1001,
+                createdAt: '2002.02.02',
+                type: [ArticleType.IT],
+                blocks: [],
+            },
+        ];
+
+        expect(
+            articlesListReducer(
+                state as ArticlesListSchema,
+                fetchArticles.fulfilled(articles, '', { page: 1, limit: 9 })
+            )
+        ).toEqual({
+            isLoading: false,
+            entities: {
+                1: articles[0],
+            },
+            ids: ['1'],
+            hasMore: true,
+        });
+    });
+
+    test('fetch articles fulfilled without articles', () => {
+        const state: DeepPartial<ArticlesListSchema> = {
+            isLoading: true,
+            hasMore: true,
+            entities: {},
+            ids: [],
+        };
+
+        const articles: Article[] = [];
+
+        expect(
+            articlesListReducer(
+                state as ArticlesListSchema,
+                fetchArticles.fulfilled(articles, '', { page: 1, limit: 9 })
+            )
+        ).toEqual({
+            isLoading: false,
+            entities: {},
+            ids: [],
+            hasMore: false,
+        });
+    });
+
+    test('fetch articles rejected', () => {
+        const state: DeepPartial<ArticlesListSchema> = {
+            isLoading: true,
+            error: undefined,
+            hasMore: true,
+            entities: {},
+            ids: [],
+        };
+
+        expect(
+            articlesListReducer(
+                state as ArticlesListSchema,
+                fetchArticles.rejected(new Error(), '', { page: 1, limit: 3 }, 'error')
+            )
+        ).toEqual({
+            isLoading: false,
+            error: 'error',
+            hasMore: false,
+            entities: {},
+            ids: [],
+        });
+    });
+});
