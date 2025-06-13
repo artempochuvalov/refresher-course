360312f6669bf121fa109277d83555f19ab86300
artempochuvalov <artempochuvalov@gmail.com>
2025-06-13 13:50:25 +0300

    fix tests

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -4,12 +4,14 @@ import { type StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
 import { profileReducer } from 'entities/Profile';
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+import { articlesListReducer } from 'pages/Articles/model/slices/articlePageSlice';
 import { ReducersList } from 'shared/lib/hooks/useDynamicModuleLoader';
 
 const defaultReducers: ReducersList = {
     loginForm: loginReducer,
     profile: profileReducer,
     articleDetails: articleDetailsReducer,
+    articlesList: articlesListReducer,
 };
 
 export const StoreDecorator = (
